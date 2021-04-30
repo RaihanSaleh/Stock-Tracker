53ffa38cdacb9e9a256efab38e9ad81aa40407e8
rsaleh <raihan.saleh@gmail.com>
2021-04-29 23:05:17 -0400

    created sort function for stockList

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,6 +3,7 @@ import { Context } from './Context';
 
 function SearchBar() {
 
+  // const token = process.env.SANDBOX_TOKEN
   const token = 'Tpk_219118a840804c0e83a9d0c7fbc7e1bc'
   const baseURL = 'https://sandbox.iexapis.com/stable/stock/market/batch?symbols='
   const urlParams = '&filter=companyName, latestPrice, previousClose, close, open, high, low, peRatio, week52High, week52Low, ytdChange, date'
@@ -11,11 +12,14 @@ function SearchBar() {
 
   let stockTicker = ""
 
+  // records the user inputed stock ticker
   const handleChange = (e) => {
     stockTicker = e.target.value.toUpperCase()
-    console.log(stockTicker)
-    console.log(stocks)
+    console.log(stockTicker)      //remove beore submitting project
+    console.log(stocks)           //remove before submitting project
   }
+
+  // Adds stock to the stockList
   const addStock = (e) => {
     e.preventDefault()
     let tempState = [...stocks]
@@ -27,14 +31,18 @@ function SearchBar() {
     .then(res => {
       let latestPrice = res[stockTicker].quote.latestPrice
       let monthPrice = res[stockTicker].chart[0].close
+      // let weekPrice = res[stockTicker].chart[0].close
       let dayPrice = res[stockTicker].quote.previousClose
 
-      let dayPercent = (latestPrice - dayPrice)/dayPrice * 100
-      let monthPercent = (latestPrice - monthPrice)/monthPrice * 100
+      let dayPerformance = (latestPrice - dayPrice)/dayPrice * 100
+      // let weekPerformance = (latestPrice - weekPrice)/weekPrice * 100
+      let monthPerformance = (latestPrice - monthPrice)/monthPrice * 100
 
+      res.symbol = stockTicker
       res.name = res[stockTicker].quote.companyName
-      res.dayPercent = +dayPercent.toFixed(2)
-      res.monthPercent = +monthPercent.toFixed(2)
+      res.dayPerformance = +dayPerformance.toFixed(2)
+      // res.weekPerformance = +weekPerformance.toFixed(2)
+      res.monthPerformance = +monthPerformance.toFixed(2)
 
       tempState.push(res)
       setStocks(tempState)
--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
+import { Context } from './Context';
 
 function StockList() {
 
+  const {stocks, setStocks} = useContext(Context)
+  const [sortBy, setSortBy] = useState('dayPerformance')
+
+  const sortStocks = (sortPram) => {
+    let sortedStocks = [...stocks]
+    sortedStocks.sort((a,b) => b[sortPram] - a[sortPram])
+    setStocks(sortStocks)
+  }
+  // this isn't working keep looping forever
+  // useEffect(() => {
+  //   sortStocks(sortBy)
+  // }, [])
+
+  let stockList = stocks.map(stock => {
+    let stockTicker = stock.symbol
+    return (
+      <div key={stock.symbol}>
+        <h4>{stock.symbol}</h4>
+        <p>{stock.name}</p>
+        <p>${stock[stockTicker].quote.latestPrice.toFixed(2)}</p>
+        <p>{stock[sortBy]}%</p>
+      </div>
+    )
+  })
+
   return (
     <div>
-
+      <button id="dayPerformance" onClick={setSortBy('dayPerformance')}>Day</button>
+      <button id="weekPerformance" onClick={setSortBy('dayPerformance')}>Week</button>
+      <button id="monthPerformance" onClick={setSortBy('dayPerformance')}>Month</button>
+      {stockList}
     </div>
   );
 }
